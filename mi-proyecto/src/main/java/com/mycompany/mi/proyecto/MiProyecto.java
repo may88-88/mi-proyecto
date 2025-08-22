17f73bc5cb31c1af64582a5ea2470b593c9a0eec
mayo88-88 <my.intriagop@uea.edu.ec>
2025-08-22 16:39:11 -0500

    Modificando el mensaje

---
+++ mi-proyecto/src/main/java/com/mycompany/mi/proyecto/MiProyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mi.proyecto;
+
+/**
+ *
+ * @author MARILYN INTRIAGO
+ */
+public class MiProyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Marilyn!");
+    }
+}
